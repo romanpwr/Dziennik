301d8c2e4aafe02eedbfce499d05a7974d982da8
Bartlomiej <sneven@gmail.com>
2012-11-21 17:23:46 +0100

    panel cz1

--- adminReportPage.php
+++ adminReportPage.php
@@ -11,10 +11,27 @@ $(document).ready(function() {
             contentType: 'application/json; charset=utf-8', 
             url: 'load.php?a=zgloszenie',                        
             success: function(zgloszenie){                       
-                     $('.report').append(zgloszenie);    
+                     $('.report').append(zgloszenie); 
 					 load_report();
                      }
   });
+  /**function filter(){
+  $('.reportFiltr').change(function(){
+	$('.reportFiltr option:selected').each(function(){
+		Filtr = $(this).attr('value');
+		 $.ajax({
+				 type: 'get',
+				 contentType: 'application/json; charset=utf-8',
+				 url: 'load.php?a=zgloszenie&f1='+Filtr,
+				 dataType: 'json',
+				 success: function(zgloszenie){
+						  $('.report').append(zgloszenie);
+						  load_report();
+						  }
+	});
+	**/
+  
+  
   
   function load_report() {
   $('.report').change(function(){                      
@@ -33,7 +50,38 @@ $(document).ready(function() {
       });
   }).trigger('change'); 
 }
+/*
+	$('.report').change(function(){
+	  var str = "";
+          $(".report option:selected").each(function () {
+                str += $(this).text() + " ";
+              });
+          $("span").text(str);
+	
+	}).change();
+*/
+	$('.reportFiltr2').change(function(){
 
+		var target = $('#filtr2').val();
+		if ( target != "ALL"){
+		$('.report').children('option').hide();
+		$('.report').children('option[value="'+target+'"]').show(); 
+		}
+		else{
+		$('.report').children('option').show();
+     	}
+	}).change();
+	$('.reportFiltr').change(function(){
+		var target = $('#filtr1').val();
+		if (target != 'all'){
+		$('.report').children('option').hide();
+		$('.report').children('option[zigi*="'+target+'"]').show(); 
+		}
+		else{
+		$('.report').children('option').show();
+		}
+		
+	}).change();
 });
 </script>
 </head>
@@ -41,17 +89,27 @@ $(document).ready(function() {
  
 <div id="container">
 	<div id="filtry">
-	    <select class="reportFiltr" style="width:200px">
-		<option>Sortowanie1</option>
-		<option>Sortowanie2</option>
-		<option>Sortowanie3</option>
-		<option>Sortowanie4</option>
+	    <select class="reportFiltr" id="filtr1" name="filtr1" style="width:200px">
+		<option id="all" value="all">Wszystkie</option>
+		<option id="new_diary" value="dodanie dziennika">Dodanie dziennika</option>
+		<option id="błąd" value="błędny komentarz">Błędny komentarz</option>
+		<option id="błąd2" value="błędny wpis">Błędny wpis</option>
+		<option id="usun" value="usunięcie konta">Usunięcie konta</option>
+		<option id="usun2" value="usunięcie dziennika">Usunięcie dzienika</option>
+		<option id="zgl" value="zgłoszenie użytkownika">Zgłoszenie użytkownika</option>
+		<option id="inne" value="inne">Inne</option>
+	</select>
+	<select class="reportFiltr2" name="filtr2" id="filtr2" style="width:200px">
+		<option id="all" value="ALL">Wszystkie</option>
+		<option id="new" value="NEW">Nowe</option>
+		<option id="otwarte" value="OTWARTE">Otwarte</option>
+		<option id="zamkniete" value="ZAMKNIETE">Zamknięte</option>
 	</select>
 	</div>
 	<form name="reportReact" action="" method="POST">
     <div id="listaZgloszen">
-    <select class="report" style="width:200px" size="20">
-		<option></option>
+    <select class="report" style="width:300px" size="20">
+		
 	</select>
     </div>
 	
@@ -64,10 +122,8 @@ $(document).ready(function() {
 	<div id="reakcja" style="float:rigth">
 	
 	<select class="option" style="width:200px" name="option">
-		<option>Sortowanie1</option>
-		<option>Sortowanie2</option>
-		<option>Sortowanie3</option>
-		<option>Sortowanie4</option>
+		<option>Wybierz działanie</option>
+		
 	</select>
 		<input type="submit" name="zglAction" value ="Wykonaj">
 		
@@ -76,7 +132,7 @@ $(document).ready(function() {
 	</div>
 	
 </div>
-
+<span></span>
 
 </body>
 
--- load.php
+++ load.php
@@ -1,9 +1,50 @@
 ﻿<?php
 include("connection.php");
 if(isset($_GET['a']) && $_GET['a'] == 'zgloszenie'){
+	if (isset($_GET['f1'])){
+	$filtr = $_GET['f1'];
+	}
+	if (isset($_GET['f2'])){
+	$filtr2 = $_GET['f2'];
+	switch ($filtr2){
+		case "OTWARTE":
+			$spr = 1;
+			break;
+		case "NEW":
+			$spr = 0;
+			break;
+		case "ZAMKNIETE":
+			$spr = 2;
+			break;
+		case "ALL";
+			break;
+	}
+	}
+	if (isset($spr) && isset($filtr)){
+	$query = mysql_query("SELECT * FROM zgloszenia WHERE Temat ='".$filtr."' AND StatusZgl ='".$spr."' ORDER BY `IdZgloszenia` ASC");
+	}
+	elseif (isset ($filtr)){
+	$query = mysql_query("SELECT * FROM zgloszenia WHERE Temat ='".$filtr."' ORDER BY `IdZgloszenia` ASC");
+	}
+	elseif (isset ($spr)){
+	$query = mysql_query("SELECT * FROM zgloszenia WHERE StatusZgl ='".$spr."' ORDER BY `IdZgloszenia` ASC");
+	}
+	else{
 	$query = mysql_query("SELECT * FROM zgloszenia ORDER BY `IdZgloszenia` ASC");
+	}
 	while ($row = mysql_fetch_array($query, MYSQL_BOTH)){
-		$zgloszenie.= '<option id="'.$row['IdZgloszenia'].'" value="'.$row['IdZgloszenia'].'">'.$row['Temat'].'</option>';
+	    switch($row['StatusZgl']){
+			case 0:
+				$stan = "NEW";
+				break;
+			case 1:
+				$stan = "OTWARTE";
+				break;
+			case 2:
+				$stan = "ZAMKNIETE";
+				break;
+		}
+		$zgloszenie.= '<option id="'.$row['IdZgloszenia'].'" zigi="'.$row['Temat'].'"name="IdZgl" value="'.$stan.'">['.$stan.'] '.$row['Temat'].'</option>';
 	}
 echo $zgloszenie;
 }
